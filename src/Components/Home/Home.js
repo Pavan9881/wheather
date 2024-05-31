0755e19086e1bdb38e2acf22ef61c16037415915
pavan <kumar@pavans-MacBook-Air.local>
2024-05-31 17:35:42 +0530

    Name

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -242,7 +242,7 @@ const Home = () => {
           />
         </View>
         <TouchableOpacity style={styles.btn} onPress={() => getData()}>
-          <Text style={styles.btnText}>Search</Text>
+          <Text style={styles.btnText}>Current Citt</Text>
         </TouchableOpacity>
         {!isConnected && (
           <TouchableOpacity
